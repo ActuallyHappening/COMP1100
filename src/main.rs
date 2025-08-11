6192ed4ff753be74e487772bd372e65cfd0bb6e3
Caleb Yates <calebyates42@gmail.com>
2025-08-11 05:16:35 +0000

    add

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use url::Url;
+use ystd::prelude::*;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Bachelor {
+	MathsCompSci = 2497,
+	Maths = 2460,
+}
+
+impl Bachelor {
+	pub fn code(&self) -> u32 {
+		*self as u32
+	}
+
+	pub fn url(&self) -> Url {
+		Url::parse(&format!(
+			"https://programs-courses.uq.edu.au/requirements/program/{}/2026",
+			self.code()
+		))
+		.unwrap()
+	}
+}
+
+impl Bachelor {
+	pub async fn get_related_courses(&self) -> color_eyre::Result<Vec<Course>> {
+		todo!()
+	}
+}
+
+pub struct Course {
+	pub code: Box<str>,
+	pub name: Box<str>,
+}
+
 fn main() {
-    println!("Hello, world!");
+	println!("Hello, world!");
 }
