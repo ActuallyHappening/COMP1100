a0b27bf0d35946e9ab798b36b58630255e6ab02b
Caleb Yates <calebyates42@gmail.com>
2025-09-10 15:12:26 +1000

    add

---
+++ code/website/src/components/StudentNumberForm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const studentNumber = ref(10000000);
+function next() {
+	console.log();
+}
+</script>
+
+<template>
+	<h1>Your Student Number?</h1>
+	<form @submit="next">
+		<input
+			:value="studentNumber"
+			type="number"
+			min="10000000"
+			max="99999999"
+		/>
+	</form>
+</template>
+
+<style scoped></style>
