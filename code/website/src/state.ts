29603f6977c6411ff72e647321a6e7ccfce58d94
Caleb Yates <calebyates42@gmail.com>
2025-09-24 05:37:41 +0000

    cy(feat): Can now connect to the db

--- code/website/src/state.ts
+++ code/website/src/state.ts
@@ -1,3 +1,7 @@
+/**
+ * This file manages the network and localStorage state
+ */
+
 import { useStorage } from "@vueuse/core";
 
 export const localState = useStorage(`student-info`, {
@@ -17,24 +21,35 @@ import { Surreal, Table } from "surrealdb";
 const db = new Surreal();
 
 // Open a connection and authenticate
+let dbPassword;
+if (typeof process !== "undefined") {
+	// assuming running node directly
+	dbPassword = process.env.COMP1100_PASSWORD;
+} else {
+	// assuming this is running in the browser
+	dbPassword = import.meta.env.VITE_COMP1100_PASSWORD;
+}
 await db.connect(
 	"wss://eager-bee-06aqohg53hq27c0jg11k14gdbk.aws-use1.surreal.cloud",
 	{
 		namespace: "comp1100",
 		database: "master",
-		auth: {
-			username: "comp1100-team10",
-			password: process.env.COMP1100_PASSWORD,
-		},
+		// auth: {
+		// 	username: "im-lazy-user",
+		// 	password: dbPassword,
+		// },
 	},
 );
 
-// read file query.surql
-import { readFile } from "fs/promises";
+import query from "../query.surql?raw";
+console.log(query);
 
-const query = await readFile("./query.surql", "utf8");
+// // read file query.surql
+// import { readFile } from "fs/promises";
 
-const resp = await db.query(query);
-console.log(`resp`, resp);
+// const query = await readFile("./query.surql", "utf8");
 
-export const netState = null;
+// const resp = await db.query(query);
+// console.log(`resp`, resp);
+
+// export const netState = null;
