7cb33d2fb0bd6de55e8d2e34b233c67619583b53
Rafael Chase <Rafael.Chase@student.uq.edu.au>
2025-10-20 11:28:30 +1000

    RC removed redundancies

--- code/website/src/components/Home.vue
+++ code/website/src/components/Home.vue
@@ -70,23 +70,6 @@ const top_level_selected = reactive({} as { [key: number]: string });
 		</div>
 	</div>
 
-	<!--
-	<div class="container-fluid">
-		<label for="plan-name">Plan name:</label>
-		<input type="text" id="plan-name" v-model="planState().name" />
-		<select
-			name="plan-current"
-			id="plan-current"
-			v-model="localState.current"
-		>
-			<option v-for="plan in Object.keys(localState.plans)" :value="plan">
-				{{ localState.plans[plan].name }}
-			</option>
-		</select>
-		<button class="btn" @click="newPlan">New Plan</button>
-	</div>
-	-->
-
 	<div class="container-fluid p-0">
 		<form action="#" @submit.prevent="() => {}">
 			<div class="input-group">
@@ -150,34 +133,6 @@ const top_level_selected = reactive({} as { [key: number]: string });
 		</div>
 	</div>
 
-	<!--<div class="container">
-		<div id="div_1">
-			<h1>This is where the courses for selection will be placed</h1>
-			<div
-				v-for="(reqlist, index) in getCurrentProgram()
-					?.program_requirements"
-				:key="!reqlist[0] ? undefined : reqlist[0].id.toString()"
-			>
-				<ProgramReq
-					:index="index"
-					@selected="(req) => (top_level_selected[index] = req)"
-				/>
-				<ProgramReqs
-					:requirement-id="top_level_selected[index]"
-					v-if="top_level_selected[index]"
-				/>
-			</div>
-		</div>
-		<div id="div_2">
-			<h1>This is where the courses will be placed</h1>
-			<div class="container">
-				<h2 id="div_3">Semester 1</h2>
-			</div>
-			<div class="container">
-				<h2 id="div_4">Semester 2</h2>
-			</div>
-		</div>
-	</div>-->
 </template>
 
 <style scoped>
@@ -198,25 +153,5 @@ const top_level_selected = reactive({} as { [key: number]: string });
 	width: 100%;
 	align-items: center;
 	margin: 0;
-}
-
-#div_1 {
-	background-color: firebrick;
-	width: 33%;
-}
-
-#div_2 {
-	background-color: #51247a;
-	flex-grow: 1;
-}
-
-#div_3 {
-	background-color: pink;
-	width: 100%;
-}
-
-#div_4 {
-	background-color: navajowhite;
-	width: 100%;
 }
 </style>
