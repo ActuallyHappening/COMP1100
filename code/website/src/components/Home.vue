fac19ca3c0890910262f17edb69b350748409aa3
TNTree <118268880+TNTreee@users.noreply.github.com>
2025-10-22 15:25:55 +1000

    PE: add information sidebar hml framework

--- code/website/src/components/Home.vue
+++ code/website/src/components/Home.vue
@@ -145,7 +145,7 @@ function courseChange(event: Event) {
 	</div>
 
 	<div class="container-fluid row" id="parent-div">
-		<div class="col-4">
+		<div class="col-3">
 			<!-- Tabs -->
 
 			<div class="nav nav-tabs" id="nav-tab" role="tablist">
@@ -184,10 +184,32 @@ function courseChange(event: Event) {
 		<div class="col-7" id="plan">
 			<PlannerVisuals />
 		</div>
-		<!-- <div class="col-1" id="info">
-
-		</div> -->
-	</div>
+		<div class="col-2"> <!-- Put this in a new vue file probably -->
+			<a class="mb-2 " href="https://programs-courses.uq.edu.au/course.html?course_code=SCIE1000&offer=53544c554332494e"> <!-- this links to sem 2, 2025. The 5th last character (2) denotes semester #, for different years append &year=2026 etc -->
+				<h3 class="text-center">SCIE1000</h3>
+				<p class="text-center">Theory & Practice in Science</p>
+			</a>
+			<ul>
+				<li>
+					<h5><strong>Semesters offered: <span>&#9432;</span></strong>	</h5> <!-- On hover show disclaimer -->
+					<p>SCIE1000 is available in the following semesters: Semester 1, Semester 2</p>
+				</li>
+				<li>
+					<h5><strong>Prerequisites:</strong></h5>
+					<p>SCIE1000 has no prerequisite courses.</p>
+					<p>You have completed all necessary prerequisites to begin this course.</p>
+				</li>
+				<li>
+					<h5><strong>Incompatibilities:</strong></h5> <!-- shows up optionally? -->
+					<p>SCIE1000 is incompatible with SCIE1100.</p>
+					<!-- <p>Please </p> -->
+				</li>
+				<li>
+					<h5><strong>Credits:</strong></h5>
+					<p>This course counts _ towards _.</p>
+				</li>
+			</ul>
+		</div>
 
 </template>
 
