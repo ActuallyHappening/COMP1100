0257e9511712718b5b8e48af14ca466781251cf1
Caleb Yates <calebyates42@gmail.com>
2025-10-26 11:16:40 +0000

    cy(feat): Searching is now implemented

--- code/website/src/apis/filter.ts
+++ code/website/src/apis/filter.ts
@@ -25,7 +25,48 @@ export const filterAPI = {
 		message: string | undefined;
 	} {
 		// TODO
-		return { courses, message: "TODO" };
+		let remaining_courses = courses
+			.filter((course) => {
+				if (filters.value.sem_1 && course.sem_1 === false) {
+					return false;
+				}
+				return true;
+			})
+			.filter((course) => !(filters.value.sem_2 && !course.sem_2))
+			.filter((course) => !(filters.value.sem_summer && !course.sem_summer))
+			.filter((course) => {
+				if (!filters.value.search) {
+					return true;
+				}
+				const str = course.name.toLowerCase() + "|" + course.code.toLowerCase();
+				return str.includes(filters.value.search.toLowerCase());
+			});
+
+		// prioritize course codes that match search first, then name
+		const codeRemaining = remaining_courses.filter((course) => {
+			if (!filters.value.search) {
+				return true;
+			}
+			return course.code
+				.toLowerCase()
+				.includes(filters.value.search.toLowerCase());
+		});
+		const nameRemaining = remaining_courses.filter((course) => {
+			if (!filters.value.search) {
+				return true;
+			}
+			return course.name
+				.toLowerCase()
+				.includes(filters.value.search.toLowerCase());
+		});
+		remaining_courses = _.uniq([...codeRemaining, ...nameRemaining]);
+
+		let filterMessage = "";
+		if (courses.length !== remaining_courses.length) {
+			const dif = courses.length - remaining_courses.length;
+			filterMessage = `Filtered ${dif} courses out`;
+		}
+		return { courses: remaining_courses, message: filterMessage };
 	},
 	clear() {
 		filters.value = defaultFilter();
