c4baa0ae36a488218b5bc3f87ff0f36207d9a57f
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2025-10-27 20:42:05 +1000

    cy(add): scraper stuff

--- code/scrapper/src/scraper/course.rs
+++ code/scrapper/src/scraper/course.rs
@@ -1,4 +1,13 @@
-use crate::{db::Course, prelude::*, scraper::CourseCode};
+use tendril::TendrilSink as _;
+
+use crate::{
+	db::Course,
+	prelude::*,
+	scraper::{
+		CourseCode,
+		html::{self, NodeData},
+	},
+};
 
 impl CourseCode {
 	pub fn url(&self) -> Url {
@@ -20,6 +29,26 @@ pub async fn scrape_course(course: CourseCode) -> color_eyre::Result<Course> {
 		let path = Utf8PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/course.html"));
 		fs::write(path, &res).await?;
 	}
+
+	let dom = html5ever::parse_document(html::RcDom::default(), html5ever::ParseOpts::default())
+		.from_utf8()
+		.read_from(&mut res.as_bytes())
+		.wrap_err("Couldn't parse")?;
+
+	let info_panel = dom.descendants(dom.document.clone()).filter(|child| {
+		if let NodeData::Element { name, attrs, .. } = &child.data {
+			if &name.local == "div" {
+				let class = attrs
+					.borrow()
+					.iter()
+					.find(|attr| &attr.name.local == "class");
+				if let Some(class) = class {
+					if class.value == ""
+				}
+			}
+		}
+		false
+	});
 
 	todo!()
 }
