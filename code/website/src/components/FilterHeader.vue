8d2b8bdfd88607cb858a50f3ae33e0963cf33929
TNTree <118268880+TNTreee@users.noreply.github.com>
2025-10-27 11:54:34 +1000

    PE: temp style change to make presentable

--- code/website/src/components/FilterHeader.vue
+++ code/website/src/components/FilterHeader.vue
@@ -3,19 +3,25 @@ import { filters, filterAPI } from "../apis/filter";
 </script>
 <template>
 	<div class="input-group">
-		<label for="top-level-search">Search for courses:</label>
-		<input type="search" id="top-level-search" v-model="filters.search" />
+		<span class="input-group-text" for="top-level-search">Search for courses:</span>
+		<input class="form-control" type="search" id="top-level-search" v-model="filters.search" />
 
 		<fieldset>
 			<legend>Semesters offered:</legend>
 		</fieldset>
-		<label for="top-level-sem-1">Sem 1:</label>
-		<input type="checkbox" v-model="filters.sem_1" />
-		<label for="top-level-sem-2">Sem 2:</label>
-		<input type="checkbox" v-model="filters.sem_2" />
-		<label for="top-level-sem-summer">Sem Summer:</label>
-		<input type="checkbox" v-model="filters.sem_summer" />
+		<span class="input-group-text" for="top-level-sem-1">Sem 1:</span>
+		<div class="input-group-text">
+			<input class="form-check-input" type="checkbox" v-model="filters.sem_1" />
+		</div>
+		<span class="input-group-text" for="top-level-sem-2">Sem 2:</span>
+		<div class="input-group-text">
+			<input class="form-check-input" type="checkbox" v-model="filters.sem_2" />
+		</div>
+		<span class="input-group-text" for="top-level-sem-summer">Sem Summer:</span>
+		<div class="input-group-text">
+			<input class="form-check-input" type="checkbox" v-model="filters.sem_summer" />
+		</div>
 
-		<button @click="filterAPI.clear">Clear Filters</button>
+		<button class="btn btn-outline-secondary" cl@click="filterAPI.clear">Clear Filters</button>
 	</div>
 </template>
