f5e38861e44028f25ee0f0e50e2142ee44f91380
TNTree <118268880+TNTreee@users.noreply.github.com>
2025-11-06 10:21:52 +1000

    PE: RHS cosmetic changes and progressbar

--- code/website/src/components/RightPanel.vue
+++ code/website/src/components/RightPanel.vue
@@ -28,6 +28,10 @@ function getFullProgram(id: string) {
 		}
 	}
 }
+
+function progress(){
+	return ((cpAPI.getCourseAssignments()[1] / programAPI.getCurrent().required_cp)*100).toFixed(0);
+}
 </script>
 <template>
 	<Course
@@ -38,53 +42,63 @@ function getFullProgram(id: string) {
 		type="summary"
 	/>
 	<template v-if="panelNotVisible()" class="rhs">
-		<div class="justify-content-center">
+		<div class="justify-content-center text-center">
 			<h3>{{ programAPI.getCurrent()?.name }}</h3>
-			<br />
 			<p>Commencing {{ Object.keys(planAPI.getCurrent().planner)[0] }}</p>
 		</div>
 		<br />
 		<template v-if="planAPI.getCurrent().topLevelReqsSelected">
 			<h4>Program Requirements</h4>
+			<ul>
 			<template v-if="!planAPI.getCurrent().topLevelReqsSelected[1]">
-				<h5><strong><i class="fa-solid fa-triangle-exclamation"></i>No major selected</strong></h5>
+				<li><h5><strong><i class="fa-solid fa-triangle-exclamation"></i> No major selected</strong></h5></li>
 			</template>
 			<template v-for="req in planAPI.getCurrent().topLevelReqsSelected">
-				<h5>
-					<i class="fa-solid fa-triangle-exclamation" 
-						v-if="cpAPI.getCourseAssignments()[0][req].achieved_cp < 
-							cpAPI.getCourseAssignments()[0][req].required_cp"
-					></i>
-					{{ getProgramReqName(req) }}:
-					{{ cpAPI.getCourseAssignments()[0][req].achieved_cp }}/{{
-						cpAPI.getCourseAssignments()[0][req].required_cp
-					}} (Max {{ cpAPI.getCourseAssignments()[0][req].max_cp }})
-				</h5>
+				<li>
+					<h5>
+						<i class="fa-solid fa-triangle-exclamation" 
+							v-if="cpAPI.getCourseAssignments()[0][req].achieved_cp < 
+								cpAPI.getCourseAssignments()[0][req].required_cp"
+						></i>
+						{{ getProgramReqName(req) }}:
+						<strong>{{ cpAPI.getCourseAssignments()[0][req].achieved_cp }}/{{
+							cpAPI.getCourseAssignments()[0][req].required_cp
+						}}</strong> (Max {{ cpAPI.getCourseAssignments()[0][req].max_cp }})
+					</h5>
 				<template v-if="getFullProgram(req).sub_requirements">
+					<ul>
 					<template
 						v-for="subreq in getFullProgram(req).sub_requirements"
 					>	
-						<p>
-							<i class="fa-solid fa-triangle-exclamation" 
-								v-if="cpAPI.getCourseAssignments()[0][subreq.id].achieved_cp < 
-									cpAPI.getCourseAssignments()[0][subreq.id].required_cp"
-							></i>
-							{{ getProgramReqName(subreq.id) }}:
-							{{
-								cpAPI.getCourseAssignments()[0][subreq.id]
-									.achieved_cp
-							}}/{{
-								cpAPI.getCourseAssignments()[0][subreq.id]
-									.required_cp
-							}} (Max {{ cpAPI.getCourseAssignments()[0][subreq.id].max_cp }})
-						</p>
+						<li>
+							<h6>
+								<i class="fa-solid fa-triangle-exclamation" 
+									v-if="cpAPI.getCourseAssignments()[0][subreq.id].achieved_cp < 
+										cpAPI.getCourseAssignments()[0][subreq.id].required_cp"
+								></i>
+								{{ getProgramReqName(subreq.id) }}:
+								<strong>{{
+									cpAPI.getCourseAssignments()[0][subreq.id]
+										.achieved_cp
+								}}/{{
+									cpAPI.getCourseAssignments()[0][subreq.id]
+										.required_cp
+								}}</strong> (Max {{ cpAPI.getCourseAssignments()[0][subreq.id].max_cp }})
+							</h6>
+						</li>
 					</template>
+					</ul>
 				</template>
+				</li>
 			</template>
+			</ul>
 		</template>
 		<br>
 		<template v-if="cpAPI.getCourseAssignments()[1] || cpAPI.getCourseAssignments()[1] === 0">
-			<h5>Total points: {{ cpAPI.getCourseAssignments()[1] }}/{{ programAPI.getCurrent().required_cp }}</h5>
+			<h5>Total points: {{ cpAPI.getCourseAssignments()[1] }}/{{ programAPI.getCurrent().required_cp }} ({{ progress() }})</h5>
+			<div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="{{ progress() }}" aria-valuemin="0" aria-valuemax="100">
+				<div class="progress-bar" v-bind:style="{ width: progress() + '%'}">{{ progress() }}%</div>
+			</div>
 		</template>
 	</template>
 </template>
